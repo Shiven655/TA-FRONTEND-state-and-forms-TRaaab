ff529e70f501018441a21a6f13f250db3b357b3f
Shiven655 <kshiven655@gmail.com>
2022-08-29 21:46:24 +0530

    basic structure of shopping cart

---
+++ block-BRaabw/shopping-cart/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Aside from './Aside';
+// import Footer from './Footer';
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <Aside />
+        {/*<Footer/> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ block-BRaabw/shopping-cart/src/components/Footer.js
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="footer">
+          <p>
+            made by...
+            <span>Shiven Bhardwaj</span>
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+export default Footer;
